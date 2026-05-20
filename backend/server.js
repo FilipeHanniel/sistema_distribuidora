680a590f7a83eaf92233e7ae8e5c1899670e6ed5
FilipeHanniel <filipehanniel@gmail.com>
2026-05-20 08:36:16 -0300

    ServerChanges

--- backend/server.js
+++ backend/server.js
@@ -772,7 +772,7 @@ app.get('/api/health', (req, res) => {
 });
 
 if (NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
   });
 }
